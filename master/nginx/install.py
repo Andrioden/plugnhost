c53749af849d2e3d0fdb1051cfdd73b9e2db6a14
André Skoglund Hansen <andre.ok@online.no>
2013-05-21 04:37:14 +0200

    install change

--- master/nginx/install.py
+++ master/nginx/install.py
@@ -1,17 +1,38 @@
+# Step 0: Do INSTALL STEPS
+"""
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
+npm install express
+"""
+
 # Step 1: Create a sites-pre-enabled directory at /etc/nginx/
+print "Creating special pre enabled directory...",
 import os
 
 directory = "/etc/nginx/sites-pre-enabled"
+#directory = "C:\\temp\\lol"
 if not os.path.exists(directory):
     os.makedirs(directory)
+print "SUCCESS"
     
-# Step 2: Copy plugnhost site defenition file to pre enabled sites directory
+# Step 2: Copy plugnhost site definition file to pre enabled sites directory
+print "Copying site definition file...",
 import shutil
 
-shutil.copy2('plugnhost', directory)
+file_dir = os.path.dirname(__file__)
+print file_dir
+filename = os.path.join(dir, '/plugnhost')
+print filename
+shutil.copy2(filename, directory)
+print "SUUCESS"
 
 # Step 3: Edit the nginx.conf file to include pre-enabled sites at the top of
 #         the http scope
+print "Editing nginx.conf file...",
 import fileinput
 
 found = False
@@ -24,3 +45,4 @@ for line in fileinput.input('test', inplace=1):
             print "\tinclude /etc/nginx/sites-pre-enabled/*;"
         found = False
     print line,
+print "SUCCESS"
