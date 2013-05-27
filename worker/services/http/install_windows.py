75c5ddd128e43f3450861ac00f8b4bcd9226e730
André Skoglund Hansen <andre.ok@online.no>
2013-05-27 20:22:24 +0200

    Smarter windows install file, also supports 64bit

--- worker/services/http/install_windows.py
+++ worker/services/http/install_windows.py
@@ -1,42 +1,51 @@
 from subprocess import call
 from tools import download
-import os
+import os, platform
+
+# Declare dependencies that has to be downloaded
+dependencies = {
+    'download': {
+        'Node.JS': {
+            '32bit': ["http://nodejs.org/dist/v0.10.8/","node-v0.10.8-x86.msi"],
+            '64bit': ["http://nodejs.org/dist/v0.10.8/x64/","node-v0.10.8-x64.msi"],
+            'call': "msiexec /i %s",
+        },
+        'Zope.Interface': {
+            '32bit': ["http://pypi.python.org/packages/2.7/z/zope.interface/","zope.interface-4.0.5.win32-py2.7.exe"],
+            '64bit': ["https://pypi.python.org/packages/2.7/z/zope.interface/","zope.interface-4.0.5.win-amd64-py2.7.exe"],
+            'call': "%s"
+        },
+        'Twisted': {
+            '32bit': ["https://pypi.python.org/packages/2.7/T/Twisted/","Twisted-13.0.0.win32-py2.7.msi"],
+            '64bit': ["https://pypi.python.org/packages/2.7/T/Twisted/","Twisted-13.0.0.win32-py2.7.msi"],
+            'call': 'msiexec /i %s'
+        }
+    },
+    'node_modules': ['express']
+}
 
 def install_windows():
-    # Step 1: Install node.js
-    node_path = download("http://nodejs.org/dist/v0.10.8/","node-v0.10.8-x86.msi")
-    if node_path:
-        print "Installing Node.JS...",
-        call('msiexec /i %s' % node_path, shell=True)
-        print "DONE (unless you cancelled, you fool?)"
-    else:
-        print "Failed to install NodeJS"
-        return
-    # Step 2: Install Node Module: Express
-    print "Installing Node Module: Express..."
+    arch_bits = platform.architecture()[0]
+    # Step 1: Install Downloadable Dependencies
+    for prog_name in dependencies['download']:
+        prog = dependencies['download'][prog_name]
+        prog_path = download(prog[arch_bits][0], prog[arch_bits][1])
+        if prog_path:
+            print "Installing %s..." % prog_name, 
+            call(prog['call'] % prog_path, shell=True)
+            print "DONE (unless you cancelled, you fool?)"
+        else:
+            print "FAILED"
+            
+    # Step 2: Install node modules
     here_path = os.path.dirname(__file__)
     node_modules_folder = os.path.join(os.path.dirname(__file__), 'node_modules')
     if not os.path.exists(node_modules_folder):
         os.makedirs(node_modules_folder)
-    call("npm install --prefix %s express" % here_path, shell=True)
-    print "...Successfully installed Node Module: Express"
-    # Step 3: Install zope.interface (twisted depends on it)
-    zope_path = download("http://pypi.python.org/packages/2.7/z/zope.interface/","zope.interface-4.0.5.win32-py2.7.exe")
-    if zope_path:
-        print "Installing zope.interface...",
-        call(zope_path, shell=True)
-        print "DONE (unless you cancelled, you fool?)"
-    else:
-        print "Failed to install zope.interface"
-        return
-    # Step 4: Install Twisted
-    twisted_path = download("https://pypi.python.org/packages/2.7/T/Twisted/","Twisted-13.0.0.win32-py2.7.msi")
-    if twisted_path:
-        print "Installing Twisted...",
-        call('msiexec /i %s' % twisted_path, shell=True)
-        print "DONE (unless you cancelled, you fool?)"
-    else:
-        print "Failed to install twisted"
+    for module_name in dependencies['node_modules']:
+        print "Installing Node Module: %s..." % module_name
+        call("npm install --prefix %s %s" % (here_path, module_name), shell=True)
+        print "...Successfully installed Node Module: %s" % module_name
         
 if __name__ == "__main__":
     install_windows()
